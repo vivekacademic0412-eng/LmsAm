2152b0a0c207043f2e0614be41519f78c4ef1787
vivekacademic0412-eng <vivekacademic.0412@gmail.com>
2026-06-16 14:36:54 +0530

    cmmit

--- resources/views/demo/layout.blade.php
+++ resources/views/demo/layout.blade.php
@@ -9,7 +9,7 @@
     {{-- <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700;800&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> --}}
     <link rel="stylesheet" href="{{ asset('theme/css/lms-demo.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> --}}
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 
