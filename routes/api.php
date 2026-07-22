f9ee233706c297f6accdd0ac2454d8d4a51980b1
vivekacademic0412-eng <vivekacademic.0412@gmail.com>
2026-07-22 11:19:02 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\LeadRegistrationController;
 use Illuminate\Support\Facades\Route;
 Route::post('/landing/register', LeadRegistrationController::class);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
